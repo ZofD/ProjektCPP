1e07abecfdc932794546899171b280188af6f12c
ZofD <lukasz.kwasny@interia.pl>
2020-11-11 15:05:52 +0100

    pierwszy commit

---
+++ cmake-build-debug/CMakeFiles/ProjektCPP.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ProjektCPP.dir/main.cpp.obj"
+  "ProjektCPP.exe"
+  "ProjektCPP.exe.manifest"
+  "ProjektCPP.lib"
+  "ProjektCPP.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ProjektCPP.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
